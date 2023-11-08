f9a98d83291519197da1ecca6c99dccde3c9713c
Celeste Kilgore <celestekilgore@gmail.com>
2023-11-08 13:33:58 -0500

    added nav docstring

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+ /** Nav: Renders navigation bar.
+  *
+  * Nav -> {Home, Companies, Jobs} */
+ 
 function Nav() {
   return (
     <ul>
